9b02364b1da4a7cf1f1cb41113e0837cffd3d5a6
Umair Ahmed <umairahmed515@gmail.com>
2023-02-21 13:23:01 +0500

    feat: don't allow deletion of client from admin panel for security reason, always have it on the tech level i.e via db access

--- client/admin.py
+++ client/admin.py
@@ -7,3 +7,6 @@ from client.models import Client
 @admin.register(Client)
 class ClientAdmin(TenantAdminMixin, admin.ModelAdmin):
     list_display = ("name", "created_on")
+
+    def has_delete_permission(self, request, obj=None):
+        return False
